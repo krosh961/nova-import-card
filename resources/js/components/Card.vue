ceb4d14e6aacbd65171b8625e7ab5b9d58c24731
Kardashov <tech@bowwoow.ru>
2019-06-26 12:27:05 +0300

    Fix Card.vue
    Fix "Import" to "{{__('Import')}}"

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,7 +1,7 @@
 <template>
     <card class="flex flex-col h-auto">
         <div class="px-3 py-3">
-            <h1 class="text-xl font-light">Import {{this.card.resourceLabel}}</h1>
+            <h1 class="text-xl font-light">{{__('Import')}} {{this.card.resourceLabel}}</h1>
             <form @submit.prevent="processImport" ref="form">
                 <div class="py-4">
                     <span class="form-file mr-4">
